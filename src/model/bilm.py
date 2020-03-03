a00d8a41c37780558f21cab8a5c7f4afc11f9b8f
zaracs <whqian001@outlook.com>
2020-03-04 01:09:51 +0800

    reduce transformer dim

--- src/model/bilm.py
+++ src/model/bilm.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-d_model = 512
+d_model = 256
 n_head = 8
-n_layer = 4
+n_layer = 6
 
 class BiLM(nn.Module):
     def __init__(self, n_vocab, n_class):
--- src/model/match.py
+++ src/model/match.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-d_model = 512
+d_model = 256
 n_head = 8
 n_layer = 6
 
