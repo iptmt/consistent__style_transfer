8d1e84a5c4f60cb5ce0cdea9ba4fdd82d052f568
zaracs <whqian001@outlook.com>
2020-03-05 18:17:25 +0800

    change structure of generator

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -1,15 +1,17 @@
 import torch
 import torch.nn as nn
+from torch.nn import Linear, LayerNorm, Dropout
+
+from torch.nn.modules import MultiheadAttention
 
 import torch.nn.functional as F
 
 
 class DenoiseTransformer(nn.Module):
     def __init__(self, n_vocab, n_class, seq_max_len, d_model=512, n_head=8, 
-                       n_enc_layer=6, n_dec_layer=6, p_dropout=0.1):
+                       n_enc_layer=4, n_dec_layer=6, p_dropout=0.1):
         super().__init__()
 
-        # self.token_embedding = nn.Linear(n_vocab, d_model, bias=False)
         self.token_embedding = nn.Embedding(n_vocab, d_model)
         self.posit_embedding = nn.Embedding(100, d_model)
         self.style_embedding = nn.Embedding(n_class, d_model)
@@ -19,7 +21,7 @@ class DenoiseTransformer(nn.Module):
         )
 
         self.decoder = nn.TransformerDecoder(
-            nn.TransformerDecoderLayer(d_model=d_model, nhead=n_head), num_layers=n_dec_layer
+            DNTransformerDecoderLayer(d_model=d_model, nhead=n_head), num_layers=n_dec_layer
         )
 
         self.proj_to_vocab = nn.Linear(d_model, n_vocab)
@@ -32,7 +34,6 @@ class DenoiseTransformer(nn.Module):
         nn.init.xavier_uniform_(self.token_embedding.weight)
     
     def encoder_embed(self, x):
-        # x_oh = F.one_hot(x, self.n_vocab).float()
         E_t = self.token_embedding(x)
         E_p = self.posit_embedding(torch.arange(x.size(1), device=x.device).long()).unsqueeze(0)
         return E_t + E_p
@@ -41,9 +42,6 @@ class DenoiseTransformer(nn.Module):
         E_s = self.style_embedding(label).unsqueeze(1)
         E_p = self.posit_embedding(torch.arange(max_len, device=label.device).long()).unsqueeze(0)
         return E_s + E_p
-    
-    # def proj_to_vocab(self, h):
-    #     return h.matmul(self.token_embedding.weight)
     
     def forward(self, x, label, max_len=None, gumbel=False, tau=1.0):
         max_len = max_len if max_len is not None else self.max_len
@@ -64,9 +62,47 @@ class DenoiseTransformer(nn.Module):
             return logits
 
 
+class DNTransformerDecoderLayer(nn.Module):
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1):
+        super(DNTransformerDecoderLayer, self).__init__()
+        self.self_attn = MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.multihead_attn = MultiheadAttention(d_model, nhead, dropout=dropout)
+        # Implementation of Feedforward model
+        self.linear1 = Linear(d_model, dim_feedforward)
+        self.dropout = Dropout(dropout)
+        self.linear2 = Linear(dim_feedforward, d_model)
+
+        self.norm1 = LayerNorm(d_model)
+        self.norm2 = LayerNorm(d_model)
+        self.norm3 = LayerNorm(d_model)
+        self.dropout1 = Dropout(dropout)
+        self.dropout2 = Dropout(dropout)
+        self.dropout3 = Dropout(dropout)
+
+    def forward(self, tgt, memory, tgt_mask=None, memory_mask=None,
+                tgt_key_padding_mask=None, memory_key_padding_mask=None):
+        # generate
+        tgt2 = self.multihead_attn(tgt, memory, memory, attn_mask=memory_mask,
+                                   key_padding_mask=memory_key_padding_mask)[0]
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm1(tgt)
+        # denoise
+        tgt2 = self.self_attn(tgt, tgt, tgt, attn_mask=tgt_mask,
+                              key_padding_mask=tgt_key_padding_mask)[0]
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm2(tgt)
+        
+        tgt2 = self.linear2(self.dropout(F.relu(self.linear1(tgt))))
+        tgt = tgt + self.dropout3(tgt2)
+        tgt = self.norm3(tgt)
+        return tgt
+
+
+
+
 if __name__ == "__main__":
     model = DenoiseTransformer(10000, 2, 16)
     x = torch.randint(0, 10000, (8, 14))
     l = torch.randint(0, 1, (8,))
-    o = model(x, l, 10)
+    o = model(x, l)
     print(o.shape)
