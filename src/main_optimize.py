7a55fcc50bac5db373eaf747d73f9e16a1f5f3ad
zaracs <whqian001@outlook.com>
2020-03-05 12:20:03 +0800

    finetune

--- src/main_optimize.py
+++ src/main_optimize.py
@@ -71,63 +71,63 @@ class GenerationTuner(pl.LightningModule):
  
     def configure_optimizers(self):
         optimizer_opt = torch.optim.Adam(self.generator.parameters(), lr=1e-4)
-        optimizer_dsc = torch.optim.Adam(self.disc.parameters(), lr=1e-4)
-        optimizer_gen = torch.optim.Adam(self.generator.parameters(), lr=1e-4)
-        return optimizer_opt, optimizer_dsc, optimizer_gen
+        # optimizer_dsc = torch.optim.Adam(self.disc.parameters(), lr=1e-4)
+        # optimizer_gen = torch.optim.Adam(self.generator.parameters(), lr=1e-4)
+        return optimizer_opt#, optimizer_dsc, optimizer_gen
     
-    def optimizer_step(self, current_epoch, batch_idx, optimizer, optimizer_idx, second_order_closure=None):
-        # update discriminator every 1 steps
-        if optimizer_idx == 1:
-            if batch_idx % 5 == 0:
-                optimizer.step()
-                optimizer.zero_grad()
-        # update generator opt every 1 steps
-        if optimizer_idx == 0 or optimizer_idx == 2:
-            optimizer.step()
-            optimizer.zero_grad()
+    # def optimizer_step(self, current_epoch, batch_idx, optimizer, optimizer_idx, second_order_closure=None):
+    #     # update discriminator every 1 steps
+    #     if optimizer_idx == 1:
+    #         if batch_idx % 5 == 0:
+    #             optimizer.step()
+    #             optimizer.zero_grad()
+    #     # update generator opt every 1 steps
+    #     if optimizer_idx == 0 or optimizer_idx == 2:
+    #         optimizer.step()
+    #         optimizer.zero_grad()
 
     def get_current_w(self):
         p = self.global_step / self.anneal_steps
         w = min([p, 1.0])
         tau = self.tau ** p
-        return self.tau, w #TODO:
+        return tau, w
 
     def training_step(self, batch, batch_idx, optimizer_idx):
         x, labels = batch
         tau, w = self.get_current_w()
         # optimize generator with estimators
-        if optimizer_idx == 0:
-            sample_p = self.forward(x, 1 - labels, self.tau, optimizer_idx)
+        # if optimizer_idx == 0:
+        sample_p = self.forward(x, 1 - labels, self.tau, 0) #TODO:
 
-            s_logits = self.classifier(sample_p)
-            c_logits = self.matcher(sample_p, x) 
-            l_logits = self.lm(sample_p, 1 - labels)
+        s_logits = self.classifier(sample_p)
+        c_logits = self.matcher(sample_p, x) 
+        l_logits = self.lm(sample_p, 1 - labels)
 
-            s_loss = self.ce_crit(s_logits, 1 - labels)
-            c_loss = self.mse_crit(c_logits, c_logits.new_full([c_logits.size(0)], self.hparams.gap))
-            l_loss = self.ce_crit(l_logits.reshape(-1, l_logits.size(-1)), sample_p.argmax(-1).reshape(-1))
+        s_loss = self.ce_crit(s_logits, 1 - labels)
+        c_loss = self.mse_crit(c_logits, c_logits.new_full([c_logits.size(0)], self.hparams.gap))
+        l_loss = self.ce_crit(l_logits.reshape(-1, l_logits.size(-1)), sample_p.argmax(-1).reshape(-1))
 
-            loss = w * (self.hparams.alpha * s_loss + self.hparams.beta * c_loss + self.hparams.gamma * l_loss)
-            loginfo = {"s": s_loss, "c": c_loss, "l": l_loss}
-            return {"loss": 1.0 * loss, "progress_bar": loginfo, "log": loginfo} #TODO:
+        loss = w * (self.hparams.alpha * s_loss + self.hparams.beta * c_loss + self.hparams.gamma * l_loss)
+        loginfo = {"s": s_loss, "c": c_loss, "l": l_loss}
+        return {"loss": 1.0 * loss, "progress_bar": loginfo, "log": loginfo} #TODO:
 
-        # optimize discriminator
-        if optimizer_idx == 1:
-            t_logits, f_logits = self.forward(x, 1 - labels, tau, optimizer_idx)
-            t_labels, f_labels = t_logits.new_ones([t_logits.size(0)]), f_logits.new_zeros([f_logits.size(0)])
-            d_loss = 0.5 * (self.bce_crit(t_logits, t_labels) + self.bce_crit(f_logits, f_labels))
-            loginfo = {"D": d_loss}
-            return {"loss": 0 * d_loss, "progress_bar": loginfo, "log": loginfo} #TODO:
+        # # optimize discriminator
+        # if optimizer_idx == 1:
+        #     t_logits, f_logits = self.forward(x, 1 - labels, tau, optimizer_idx)
+        #     t_labels, f_labels = t_logits.new_ones([t_logits.size(0)]), f_logits.new_zeros([f_logits.size(0)])
+        #     d_loss = 0.5 * (self.bce_crit(t_logits, t_labels) + self.bce_crit(f_logits, f_labels))
+        #     loginfo = {"D": d_loss}
+        #     return {"loss": d_loss, "progress_bar": loginfo, "log": loginfo}
         
-        # optimize generator
-        if optimizer_idx == 2:
-            sample_p = self.forward(x, 1 - labels, tau, optimizer_idx)
-            g_logits = self.disc(sample_p)
-            g_labels = g_logits.new_ones([g_logits.size(0)])
-            g_loss = self.bce_crit(g_logits, g_labels)
+        # # optimize generator
+        # if optimizer_idx == 2:
+        #     sample_p = self.forward(x, 1 - labels, tau, optimizer_idx)
+        #     g_logits = self.disc(sample_p)
+        #     g_labels = g_logits.new_ones([g_logits.size(0)])
+        #     g_loss = self.bce_crit(g_logits, g_labels)
 
-            loginfo = {"G": g_loss}
-            return {"loss": 0 * g_loss, "progress_bar": loginfo, "log": loginfo} #TODO:
+        #     loginfo = {"G": g_loss}
+        #     return {"loss": g_loss, "progress_bar": loginfo, "log": loginfo}
         
     def validation_step(self, batch, batch_idx):
         x, labels = batch
