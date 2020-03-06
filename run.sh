9cd01b4c34aaf18980bb0d3583c10625e171af53
zaracs <whqian001@outlook.com>
2020-03-06 22:04:09 +0800

    finetune

--- run.sh
+++ run.sh
@@ -2,19 +2,21 @@
 
 dataset=$1
 version=$2
+cuda_id=$3
 
 echo "Dataset: "$dataset""
 echo "Version: "$version""
+echo "CUDA ID: "$cuda_id""
 
-bash prepare.sh $dataset $version
+bash prepare.sh $dataset $version >> /dev/null
 
 cd ./src
 
 # train
-nohup python main_optimize.py --dataset=$dataset --ver=$version > opt_"$version".log 2>&1 &
+CUDA_VISIBLE_DEVICES=$cuda_id nohup python main_optimize.py --dataset=$dataset --ver=$version > opt_"$version".log 2>&1 &
 
 # inference
-nohup python main_optimize.py --dataset=$dataset --mode=test --ver=$version > /dev/null 2>&1 &
+CUDA_VISIBLE_DEVICES=$cuda_id nohup python main_optimize.py --dataset=$dataset --mode=test --ver=$version > /dev/null 2>&1 &
 
 # evaluate
 cd ../evaluate
