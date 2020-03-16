d799cf296ee80015c5914b5e0f4151ab154568d6
zaracs <whqian001@outlook.com>
2020-03-16 22:41:32 +0800

    finetune

--- src/arguments.py
+++ src/arguments.py
@@ -23,9 +23,8 @@ def fetch_args():
 
     parser.add_argument('--w_s', type=float, default=0.1, help="weight of CP")
     parser.add_argument('--w_c', type=float, default=1.0, help="weight of NT")
-    # parser.add_argument('--w_l', type=float, default=1.0, help="weight of STI")
 
-    parser.add_argument('--tau', type=float, default=0.01, help="annealling temperature")
+    parser.add_argument('--tau', type=float, default=0.1, help="annealling temperature")
     parser.add_argument('--gap', type=float, default=0., help="annealling temperature")
 
     parser.add_argument('--device', type=str, default="0", help="device id")
