e415a833d2f8660ddaf0c118d253ec75c7065bb5
zaracs <whqian001@outlook.com>
2020-03-22 04:14:01 +0800

    finetune

--- src/model/discriminator.py
+++ src/model/discriminator.py
@@ -17,9 +17,9 @@ class RelGAN_D(nn.Module):
         self.emb_dim_single = int(embed_dim / num_rep)
 
         self.embeddings = nn.Linear(vocab_size, embed_dim, bias=False)
-        self.style_embedding = nn.Embedding(2, embed_dim)
 
-        nn.init.uniform_(self.style_embedding.weight, -0.01, 0.01)
+        # self.style_embedding = nn.Embedding(2, embed_dim)
+        # nn.init.uniform_(self.style_embedding.weight, -0.01, 0.01)
 
         self.convs = nn.ModuleList([
             nn.Conv2d(1, n, (f, self.emb_dim_single), stride=(1, self.emb_dim_single)) for (n, f) in
@@ -40,7 +40,7 @@ class RelGAN_D(nn.Module):
         :return logits: [batch_size * num_rep] (1-D tensor)
         """
         emb = self.embeddings(inp).unsqueeze(1)  # batch_size * 1 * max_seq_len * embed_dim
-        emb = emb + self.style_embedding(labels).unsqueeze(1).unsqueeze(1)
+        # emb = emb + self.style_embedding(labels).unsqueeze(1).unsqueeze(1)
 
         cons = [F.relu(conv(emb)) for conv in self.convs]  # [batch_size * num_filter * (seq_len-k_h+1) * num_rep]
         pools = [F.max_pool2d(con, (con.size(2), 1)).squeeze(2) for con in cons]  # [batch_size * num_filter * num_rep]
