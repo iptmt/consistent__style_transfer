ac0f6ae118e50df60280552cba353e3089a31099
zaracs <whqian001@outlook.com>
2020-03-21 16:15:25 +0800

    fintune

--- src/arguments.py
+++ src/arguments.py
@@ -21,7 +21,7 @@ def fetch_args():
     parser.add_argument('--n_class', type=int, default=2, help="number of styles")
     parser.add_argument('--p_drop', type=float, default=0.1, help="dropout rate")
 
-    parser.add_argument('--w_s', type=float, default=0.05, help="weight of STI")
+    parser.add_argument('--w_s', type=float, default=0.1, help="weight of STI")
     parser.add_argument('--w_c', type=float, default=0.5, help="weight of CP")
 
     parser.add_argument('--tau', type=float, default=0.1, help="annealling temperature")
