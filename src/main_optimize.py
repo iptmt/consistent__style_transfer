1386f6fcf261379dcc4065b8adc0c5a99d730b8d
zaracs <whqian001@outlook.com>
2020-03-29 11:12:49 +0800

    disable reloading generation

--- src/main_optimize.py
+++ src/main_optimize.py
@@ -43,7 +43,8 @@ class GenerationTuner(pl.LightningModule):
         self.denoiser.load_state_dict(torch.load(f"{args.dump_dir}/{args.dataset}/pretrain/dn.pth"))
 
         if args.mode == "train":
-            self.generator.load_state_dict(torch.load(f"{args.dump_dir}/{args.dataset}/warmup/G.pth"))
+            # self.generator.load_state_dict(torch.load(f"{args.dump_dir}/{args.dataset}/warmup/G.pth"))
+            pass
         elif args.mode == "test":
             files = os.listdir(args.task_dump_dir)
             if len(files) > 0:
