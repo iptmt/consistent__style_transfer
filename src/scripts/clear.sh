47bbee77d8311cdcbaeef2e2c055a9681e59be8e
zaracs <whqian001@outlook.com>
2020-04-07 16:27:50 +0800

    none

--- src/scripts/clear.sh
+++ src/scripts/clear.sh
@@ -2,7 +2,7 @@
 echo "dataset: $1"
 echo "version: $2"
 
-rm -rf ../dump/"$1"/optimize-"$2"
-rm -rf ../log/"$1"/optimize-"$2"
-rm -rf ../evaluate/eval_dump/adv_models/unigram_lr_"$2"_"$1".bin
-rm -rf ../output/"$1"-"$2"
+rm -rf ../../dump/"$1"/optimize-"$2"
+rm -rf ../../log/"$1"/optimize-"$2"
+rm -rf ../../evaluate/eval_dump/adv_models/unigram_lr_"$2"_"$1".bin
+rm -rf ../../output/"$1"-"$2"
