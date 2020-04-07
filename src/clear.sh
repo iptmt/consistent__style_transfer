d8fa8c24224c36aef891d44ad7d71bb9084a5865
zaracs <whqian001@outlook.com>
2020-04-07 16:26:24 +0800

    add clear script

--- src/clear.sh
+++ src/clear.sh
@@ -1,5 +1,8 @@
-rm -rf ../dump/yelp/optimize*
-rm -rf ../log/yelp/optimize*
-rm -rf *.log
-rm -rf ../evaluate/eval_dump/adv_models/*
-rm -rf ../output/*
+#!/bin/bash
+echo "dataset: $1"
+echo "version: $2"
+
+rm -rf ../dump/"$1"/optimize-"$2"
+rm -rf ../log/"$1"/optimize-"$2"
+rm -rf ../evaluate/eval_dump/adv_models/unigram_lr_"$2"_"$1".bin
+rm -rf ../output/"$1"-"$2"
