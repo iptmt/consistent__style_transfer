0eabf780a9ebf6e51c444db1d2cefc5d7b218b9f
zaracs <whqian001@outlook.com>
2020-05-22 23:07:41 +0800

    bugfix

--- src/scripts/bt_format.py
+++ src/scripts/bt_format.py
@@ -19,6 +19,8 @@ file_names = os.listdir(in_dir)
 
 # transform
 for name in file_names:
+    if "reference" in name:
+        continue
     lines = read(in_dir + name)
     lines = [name.split(".")[-1] + " " + l for l in lines]
     write(lines, out_dir + name)
@@ -28,11 +30,11 @@ lines_0 = ["0" + " " + l for l in read(in_dir + "style.train.0")]
 lines_1 = ["1" + " " + l for l in read(in_dir + "style.train.1")]
 lines_train = lines_0 + lines_1
 random.shuffle(lines_train)
-write(lines_train, in_dir + "train.txt")
+write(lines_train, out_dir + "train.txt")
 
 # merge dev
 lines_0 = ["0" + " " + l for l in read(in_dir + "style.dev.0")]
 lines_1 = ["1" + " " + l for l in read(in_dir + "style.dev.1")]
 lines_dev = lines_0 + lines_1
 random.shuffle(lines_dev)
-write(lines_dev, in_dir + "dev.txt")
+write(lines_dev, out_dir + "dev.txt")
